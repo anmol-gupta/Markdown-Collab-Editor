b8d11436b1a66546e1403c7d1a31be65cf8286a1
Anmol Gupta <anmolgupta@Anmols-MacBook-Pro-2.local>
2018-08-25 03:47:09 +0530

    Finished Server Side Code

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var app = express();
+var sharejs = require('share');
+
+var options = {
+    db: {type: 'redis', client: redisClient}
+};
+sharejs.server.attach(app, options);
+
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+  redisClient = require("redis").createClient();
+}
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+    res.render('pad');
+})
+
+app.get('/(:id)', (req, res) => {
     res.render('pad');
 })
 
--- public/script.js
+++ public/script.js
@@ -2,14 +2,54 @@ window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
     var markdownArea = document.getElementById('markdown');   
+    var previousMDvalue;
 
-    var convertTextAreaToMarkdown = function(){
+    pad.addEventListener('keydown',(e) => {
+        if(e.keyCode === 9) { // tab was pressed
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            target.value = value.substring(0, start)
+                            + "\t"
+                            + value.substring(end);
+
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            e.preventDefault();
+        }
+    });
+
+    var convertTextAreaToMarkdown = () => {
         var markdownText = pad.value;
         html = converter.makeHtml(markdownText);
         markdownArea.innerHTML = html;
     };
 
+    var didChangeOccur = () => {
+        if(previousMDvalue!=pad.value){
+            return true;
+        }
+        return false;
+    }
+
+    setInterval(() => {
+        if(didChangeOccur){
+            convertTextAreaToMarkdown();
+        }
+    }, 1000);
+
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
+    if(document.location.pathname.length > 1){
+        // implement share js
+        var documentName = document.location.pathname.substring(1);
+        sharejs.open(documentName, 'text', function(error, doc) {
+            doc.attach_textarea(pad);
+            convertTextAreaToMarkdown();
+        });        
+    }
     convertTextAreaToMarkdown();
 };
